eb23b432ec0e05cec7d13999b6e2340401cda787
Dheena M <mddhayalan@gmail.com>
2018-07-30 16:45:11 +0530

    adding new files

---
+++ Powershell/Tfs_commands/Tfs_commands/Get.GfnCurrent.Revision.ps1
@@ -0,0 +1,32 @@
+﻿function xmlPeek($filePath, $xpath) { 
+    [xml] $fileXml = Get-Content $filePath 
+    return $fileXml.SelectSingleNode($xpath).Value 
+} 
+
+function xmlPoke($file, $xpath, $value) { 
+    $filePath = $file 
+
+    [xml] $fileXml = Get-Content $filePath 
+    $node = $fileXml.SelectSingleNode($xpath) 
+    if ($node) { 
+        $node.Value = $value 
+
+        $fileXml.Save($filePath)  
+    } 
+}
+
+$Gfnpath = $args[0]
+Set-Location -Path $Gfnpath 
+$UADE_Revision_File = $Gfnpath + "\..\UADE_Delivery\UADE_SVN_Rev.txt"
+$UADE_Changeset_xml = $Gfnpath +"\..\UADE_Delivery\Changeset.xml"
+$exepath = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\TF.exe"
+$result = & $exepath history . /r /noprompt /stopafter:1 /version:W
+"$result" -match "\d+"
+$GfnRevision = $matches[0]
+$xpath = "/SubsytemChangeset/DependentSubsystem/@Changeset";
+
+xmlPoke $UADE_Changeset_xml $xpath $GfnRevision
+$env:GFNVersion = $GfnRevision
+$env:UADEVersion = Get-Content $UADE_Revision_File
+Write-Host "UADE Revision: "$env:UADEVersion
+Write-Host "Gfn Revision: "$env:GFNVersion
---
+++ Powershell/Tfs_commands/Tfs_commands/Tfs_Common.ps1
@@ -0,0 +1,21 @@
+﻿"Microsoft.TeamFoundation.Client",
+"Microsoft.TeamFoundation.VersionControl.Common",
+"Microsoft.TeamFoundation.VersionControl.Client" |
+    ForEach-Object { Add-Type -AssemblyName "$_, Version=14.0.0.0, Culture=Neutral, PublicKeyToken=b03f5f7f11d50a3a" }
+
+$tfsUrl = "http://tfsserver:8080/collection"
+
+$tfs = [Microsoft.TeamFoundation.Client.TeamFoundationServerFactory]::GetServer($tfsUrl)
+$vcs = $tfs.GetService([type]"Microsoft.TeamFoundation.VersionControl.Client.VersionControlServer")
+
+$workspaceParameters = New-Object Microsoft.TeamFoundation.VersionControl.Client.CreateWorkspaceParameters -ArgumentList "WorkspaceName"
+
+# Add any specific parameters that you want according to http://msdn.microsoft.com/en-us/library/microsoft.teamfoundation.versioncontrol.client.createworkspaceparameters.aspx
+# e.g. $workspaceParameters.Comment = ""
+# e.g. $workspaceParameters.Computer = ""
+# e.g. $workspaceParameters.Location = [Microsoft.TeamFoundation.VersionControl.Common.WorkspaceLocation]::Local
+
+$workspace = $vcs.CreateWorkspace($workspaceParameters)
+
+# Add any working folders that you would defined below
+# e.g. $workspace.Map("$/", "C:\ProjectDirectory")
